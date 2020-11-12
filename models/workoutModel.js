010e13394fdd416d2cbb573c2e59c4a50c5cbf6b
Zinc <greenberg.zachary@utexas.edu>
2020-11-12 17:29:20 -0600

    model js with export

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -6,7 +6,40 @@ const WorkoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    {
-        exercises:
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Type of Workout is Required"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Name for the Workout is Required"
+        },
+        duration: {
+            type: Number,
+            required: "Duration for the Workout is Required"
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }],
+    totalDuration: {
+        type: Number,
+        default: 0
     }
-})
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
