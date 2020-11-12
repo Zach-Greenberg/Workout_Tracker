88db47885eecfe59da0b3ec331d78f28ad30a954
Zinc <greenberg.zachary@utexas.edu>
2020-11-12 17:16:17 -0600

    server.js and port established

---
+++ models/workoutModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    {
+        exercises:
+    }
+})
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extrended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.use(require("./routes/apiRoutes"));
+
+app.listen(PORT, () => {
+    console.log(`Applocation running on PORT ${PORT}`);
+})
