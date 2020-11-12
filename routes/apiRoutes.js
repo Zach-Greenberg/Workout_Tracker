049d0b8816723bd2543065b033bd8aa47446457e
Zinc <greenberg.zachary@utexas.edu>
2020-11-12 17:54:40 -0600

    skeleton for routes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,33 @@
-const router = require("express");
+const router = require("express").Router();
 const Workout = require("../models/workoutModel");
 const path = require("path");
 
+router.get("/api/workouts", (req, res) => {
+
+  });
+  
+  router.get("/api/workouts/range", (req, res) => {
+
+  });
+  
+  router.put("/api/workouts/:id", (req, res) => {
+ 
+  });
+  
+  router.post("/api/workouts", ({body}, res) => {
+
+  });
+  
+  router.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
+  
+  router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  
+  router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  })
+  
+  module.exports = router;
